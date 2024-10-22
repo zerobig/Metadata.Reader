9c8f984b8dc08cee7fa889ed923d2c827d7ac822
Ilya Bushin <big@mail.ru>
2024-10-23 00:53:57 +0300

    тестовые данные

---
+++ Metadata.Reader.Tests/MetadataReaderTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Metadata.Reader.Tests;
+
+public class MetadataReaderTests
+{
+    [Fact]
+    public void ReadDesignerConfiguration()
+    {
+        // Arrange
+        var reader = new MetadataReader();
+
+        // Act
+        var configuration = reader.readMetadata("./TestFiles/Designer");
+
+        // Assert
+        // TODO:
+    }
+}
